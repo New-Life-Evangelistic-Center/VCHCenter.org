2bf7e2ef46ed6cd78296d6426486dbe16f9979e8
xCONFLiCTiONx <MichaelJ.Hall@outlook.com>
2019-10-12 15:24:28 -0500

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,309 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
+
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory)
+    } else if (typeof exports === 'object') {
+        module.exports = factory()
+    } else {
+        root.tingle = factory()
+    }
+}(this, function() {
+
+    var isBusy = false
+
+    function Modal(options) {
+        var defaults = {
+            onClose: null,
+            onOpen: null,
+            beforeOpen: null,
+            beforeClose: null,
+            stickyFooter: false,
+            footer: false,
+            cssClass: [],
+            closeLabel: 'Close',
+            closeMethods: ['overlay', 'button', 'escape']
+        }
+
+        this.opts = extend({}, defaults, options)
+        
+        this.init()
+    }
+
+    Modal.prototype.init = function() {
+        if (this.modal) {
+            return
+        }
+
+        _build.call(this)
+        _bindEvents.call(this)
+        
+        document.body.insertBefore(this.modal, document.body.firstChild)
+
+        if (this.opts.footer) {
+            this.addFooter()
+        }
+
+        return this
+    }
+
+    Modal.prototype._busy = function(state) {
+        isBusy = state
+    }
+
+    Modal.prototype._isBusy = function() {
+        return isBusy
+    }
+
+    Modal.prototype.destroy = function() {
+        if (this.modal === null) {
+            return
+        }
+
+        if (this.isOpen()) {
+            this.close(true)
+        }
+
+        _unbindEvents.call(this)
+
+        this.modal.parentNode.removeChild(this.modal)
+
+        this.modal = null
+    }
+
+    Modal.prototype.isOpen = function() {
+        return !!this.modal.classList.contains('tingle-modal--visible')
+    }
+
+    Modal.prototype.open = function() {
+        if(this._isBusy()) return
+        this._busy(true)
+
+        var self = this
+
+        if (typeof self.opts.beforeOpen === 'function') {
+            self.opts.beforeOpen()
+        }
+
+        if (this.modal.style.removeProperty) {
+            this.modal.style.removeProperty('display')
+        } else {
+            this.modal.style.removeAttribute('display')
+        }
+
+        this._scrollPosition = window.pageYOffset
+        document.body.classList.add('tingle-enabled')
+        document.body.style.top = -this._scrollPosition + 'px'
+
+        this.setStickyFooter(this.opts.stickyFooter)
+
+        this.modal.classList.add('tingle-modal--visible')
+
+        if (typeof self.opts.onOpen === 'function') {
+            self.opts.onOpen.call(self)
+        }
+
+        self._busy(false)
+        
+        this.checkOverflow()
+
+        return this
+    }
+
+    Modal.prototype.close = function(force) {
+        if(this._isBusy()) return
+        this._busy(true)
+        force = force || false
+
+        if (typeof this.opts.beforeClose === 'function') {
+            var close = this.opts.beforeClose.call(this)
+            if (!close) {
+                this._busy(false)
+                return
+            }
+        }
+
+        document.body.classList.remove('tingle-enabled')
+        window.scrollTo(0, this._scrollPosition)
+        document.body.style.top = null
+
+        this.modal.classList.remove('tingle-modal--visible')
+
+        var self = this
+
+        self.modal.style.display = 'none'
+
+        if (typeof self.opts.onClose === 'function') {
+            self.opts.onClose.call(this)
+        }
+
+        self._busy(false)
+        
+    }
+
+    Modal.prototype.setContent = function(content) {
+        if (typeof content === 'string') {
+            this.modalBoxContent.innerHTML = content
+        } else {
+            this.modalBoxContent.innerHTML = ''
+            this.modalBoxContent.appendChild(content)
+        }
+
+        if (this.isOpen()) {
+            this.checkOverflow()
+        }
+        
+        return this
+    }
+
+    Modal.prototype.getContent = function() {
+        return this.modalBoxContent
+    }
+
+    Modal.prototype.setStickyFooter = function(isSticky) {
+        if (!this.isOverflow()) {
+            isSticky = false
+        }
+
+        return this
+    }
+
+
+    Modal.prototype.isOverflow = function() {
+        var viewportHeight = window.innerHeight
+        var modalHeight = this.modalBox.clientHeight
+
+        return modalHeight >= viewportHeight
+    }
+
+    Modal.prototype.checkOverflow = function() {
+        if (this.modal.classList.contains('tingle-modal--visible')) {
+            if (this.isOverflow()) {
+                this.modal.classList.add('tingle-modal--overflow')
+            } else {
+                this.modal.classList.remove('tingle-modal--overflow')
+            }
+
+            if (!this.isOverflow() && this.opts.stickyFooter) {
+                this.setStickyFooter(false)
+            } else if (this.isOverflow() && this.opts.stickyFooter) {
+                _recalculateFooterPosition.call(this)
+                this.setStickyFooter(true)
+            }
+        }
+    }
+
+    function closeIcon() {
+        return '<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><path d="M.3 9.7c.2.2.4.3.7.3.3 0 .5-.1.7-.3L5 6.4l3.3 3.3c.2.2.5.3.7.3.2 0 .5-.1.7-.3.4-.4.4-1 0-1.4L6.4 5l3.3-3.3c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L5 3.6 1.7.3C1.3-.1.7-.1.3.3c-.4.4-.4 1 0 1.4L3.6 5 .3 8.3c-.4.4-.4 1 0 1.4z" fill="#000" fill-rule="nonzero"/></svg>'
+    }
+
+    function _build() {
+
+        this.modal = document.createElement('div')
+        this.modal.classList.add('tingle-modal')
+
+        if (this.opts.closeMethods.length === 0 || this.opts.closeMethods.indexOf('overlay') === -1) {
+            this.modal.classList.add('tingle-modal--noOverlayClose')
+        }
+
+        this.modal.style.display = 'none'
+
+        this.opts.cssClass.forEach(function(item) {
+            if (typeof item === 'string') {
+                this.modal.classList.add(item)
+            }
+        }, this)
+
+        if (this.opts.closeMethods.indexOf('button') !== -1) {
+            this.modalCloseBtn = document.createElement('button')
+            this.modalCloseBtn.type = 'button'
+            this.modalCloseBtn.classList.add('tingle-modal__close')
+
+            this.modalCloseBtnIcon = document.createElement('span')
+            this.modalCloseBtnIcon.classList.add('tingle-modal__closeIcon')
+            this.modalCloseBtnIcon.innerHTML = closeIcon()
+
+            this.modalCloseBtnLabel = document.createElement('span')
+
+            this.modalCloseBtn.appendChild(this.modalCloseBtnIcon)
+        }
+
+        this.modalBox = document.createElement('div')
+        this.modalBox.classList.add('tingle-modal-box')
+
+        this.modalBoxContent = document.createElement('div')
+        this.modalBoxContent.classList.add('tingle-modal-box__content')
+
+        this.modalBox.appendChild(this.modalBoxContent)
+
+        if (this.opts.closeMethods.indexOf('button') !== -1) {
+            this.modal.appendChild(this.modalCloseBtn)
+        }
+
+        this.modal.appendChild(this.modalBox)
+    }
+
+    function _bindEvents() {
+
+        this._events = {
+            clickCloseBtn: this.close.bind(this),
+            clickOverlay: _handleClickOutside.bind(this),
+            resize: this.checkOverflow.bind(this),
+            keyboardNav: _handleKeyboardNav.bind(this)
+        }
+
+        if (this.opts.closeMethods.indexOf('button') !== -1) {
+            this.modalCloseBtn.addEventListener('click', this._events.clickCloseBtn)
+        }
+
+        this.modal.addEventListener('mousedown', this._events.clickOverlay)
+        window.addEventListener('resize', this._events.resize)
+        document.addEventListener('keydown', this._events.keyboardNav)
+    }
+
+    function _handleKeyboardNav(event) {
+        if (this.opts.closeMethods.indexOf('escape') !== -1 && event.which === 27 && this.isOpen()) {
+            this.close()
+        }
+    }
+
+    function _handleClickOutside(event) {
+        if (this.opts.closeMethods.indexOf('overlay') !== -1 && !_findAncestor(event.target, 'tingle-modal') &&
+        event.clientX < this.modal.clientWidth) {
+            this.close()
+        }
+    }
+
+    function _findAncestor(el, cls) {
+        while ((el = el.parentElement) && !el.classList.contains(cls));
+        return el
+    }
+
+    function _unbindEvents() {
+        if (this.opts.closeMethods.indexOf('button') !== -1) {
+            this.modalCloseBtn.removeEventListener('click', this._events.clickCloseBtn)
+        }
+        this.modal.removeEventListener('mousedown', this._events.clickOverlay)
+        window.removeEventListener('resize', this._events.resize)
+        document.removeEventListener('keydown', this._events.keyboardNav)
+    }
+
+    function extend() {
+        for (var i = 1; i < arguments.length; i++) {
+            for (var key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key)) {
+                    arguments[0][key] = arguments[i][key]
+                }
+            }
+        }
+        return arguments[0]
+    }
+
+    return {
+        modal: Modal
+    }
+
+}))
 
 	var	$window = $(window),
 		$body = $('body'),
