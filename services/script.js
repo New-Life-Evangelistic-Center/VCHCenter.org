b52f62a6cd4c356614d66e2f0fa0733fd54923ec
xCONFLiCTiONx <MichaelJ.Hall@outlook.com>
2019-10-12 14:01:31 -0500

    Update

---
+++ services/script.js
@@ -0,0 +1,110 @@
+			/* Day Shelter */
+				var btn6 = document.querySelector('.js-tingle-modal-DayShelter');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-DayShelter').innerHTML);
+					modalSurprise.open();
+				});
+			/* Day Shelter - Verse */
+				var btn6 = document.querySelector('.js-tingle-modal-DayShelterVerse');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-DayShelterVerse').innerHTML);
+					modalSurprise.open();
+				});
+			/* Free Store */
+				var btn6 = document.querySelector('.js-tingle-modal-FreeStore');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-FreeStore').innerHTML);
+					modalSurprise.open();
+				});
+			/* Free Store - Verse */
+				var btn6 = document.querySelector('.js-tingle-modal-FreeStoreVerse');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-FreeStoreVerse').innerHTML);
+					modalSurprise.open();
+				});
+			/* Library */
+				var btn6 = document.querySelector('.js-tingle-modal-Library');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-Library').innerHTML);
+					modalSurprise.open();
+				});
+			/* Library - Verse */
+				var btn6 = document.querySelector('.js-tingle-modal-LibraryVerse');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-LibraryVerse').innerHTML);
+					modalSurprise.open();
+				});
+			/* Rest Area */
+				var btn6 = document.querySelector('.js-tingle-modal-RestArea');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-RestArea').innerHTML);
+					modalSurprise.open();
+				});
+			/* Rest Area - Verse */
+				var btn6 = document.querySelector('.js-tingle-modal-RestAreaVerse');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-RestAreaVerse').innerHTML);
+					modalSurprise.open();
+				});
+			/* Sleeping Area */
+				var btn6 = document.querySelector('.js-tingle-modal-SleepingArea');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-SleepingArea').innerHTML);
+					modalSurprise.open();
+				});
+			/* Sleeping Area - Verse */
+				var btn6 = document.querySelector('.js-tingle-modal-SleepingAreaVerse');
+				btn6.addEventListener('click', function(){
+					var modalSurprise = new tingle.modal({
+						onClose: function(){
+							modalSurprise.destroy();
+						}
+					});
+					modalSurprise.setContent(document.querySelector('.tingle-SleepingAreaVerse').innerHTML);
+					modalSurprise.open();
+				});
